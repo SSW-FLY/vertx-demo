49b68efb7c12a17b682a47b80c34880d71ea8fd2
nameless <ssw_fly@163.com>
2024-03-11 22:59:17 +0800

    decode

--- src/main/java/com/example/vertxwebdemo/config/ServiceRegister.java
+++ src/main/java/com/example/vertxwebdemo/config/ServiceRegister.java
@@ -1,27 +1,27 @@
-//package com.example.vertxwebdemo.config;
-//
-//import io.vertx.core.AbstractVerticle;
-//import io.vertx.core.Vertx;
-//import org.springframework.boot.ApplicationArguments;
-//import org.springframework.boot.ApplicationRunner;
-//import org.springframework.stereotype.Component;
-//
-//import javax.annotation.Resource;
-//import java.util.List;
-//
-//@Component
-//public class ServiceRegister implements ApplicationRunner {
-//
-//
-//    @Resource
-//    private Vertx vertx;
-//
-//    @Resource
-//    private List<AbstractVerticle> verticles;
-//
-//
-//    @Override
-//    public void run(ApplicationArguments args) throws Exception {
-//        verticles.forEach(x -> vertx.deployVerticle(x));
-//    }
-//}
+package com.example.vertxwebdemo.config;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component
+public class ServiceRegister implements ApplicationRunner {
+
+
+    @Resource
+    private Vertx vertx;
+
+    @Resource
+    private List<AbstractVerticle> verticles;
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        verticles.forEach(x -> vertx.deployVerticle(x));
+    }
+}
--- src/main/java/com/example/vertxwebdemo/service/MyService.java
+++ src/main/java/com/example/vertxwebdemo/service/MyService.java
@@ -1,53 +1,50 @@
-//package com.example.vertxwebdemo.service;
-//
-//import com.example.vertxwebdemo.config.VertxConfig;
-//import io.vertx.core.AbstractVerticle;
-//import io.vertx.core.Vertx;
-//import io.vertx.core.http.HttpMethod;
-//import io.vertx.core.http.HttpServer;
-//import io.vertx.core.http.HttpServerResponse;
-//import io.vertx.ext.web.Router;
-//import org.springframework.boot.ApplicationArguments;
-//import org.springframework.boot.ApplicationRunner;
-//import org.springframework.core.io.ClassPathResource;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.core.script.DefaultRedisScript;
-//import org.springframework.scripting.support.ResourceScriptSource;
-//import org.springframework.stereotype.Service;
-//
-//import javax.annotation.Resource;
-//import java.util.Collections;
-//
-//@Service
-//public class MyService extends AbstractVerticle implements ApplicationRunner {
-//
-//    @Resource
-//    private Router router;
-//
-//    @Resource
-//    private RedisTemplate<String, String> redisTemplate;
-//
-//    private DefaultRedisScript<String> script = new DefaultRedisScript<>();
-//
-//
-//    @Override
-//    public void start() throws Exception {
-//        router.route(HttpMethod.GET, "/api/test").handler(ctx -> {
-//            // 所有的请求都会调用这个处理器处理
-//            HttpServerResponse response = ctx.response();
-//            response.putHeader("content-type", "text/plain");
-//            String execute = redisTemplate.execute(script, Collections.singletonList("k"));
-//            // 写入响应并结束处理
-//            response.end(execute);
-//        });
-//    }
-//
-//    @Override
-//    public void run(ApplicationArguments args) throws Exception {
-//        ResourceScriptSource pathResource = new ResourceScriptSource(new ClassPathResource("redis/lua.lua"));
-//
-//        script.setResultType(String.class);
-//
-//        script.setScriptSource(pathResource);
-//    }
-//}
+package com.example.vertxwebdemo.service;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+
+@Service
+public class MyService extends AbstractVerticle implements ApplicationRunner {
+
+    @Resource
+    private Router router;
+
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+
+    private final DefaultRedisScript<String> script = new DefaultRedisScript<>();
+
+
+    @Override
+    public void start() throws Exception {
+        router.route(HttpMethod.GET, "/api/test").handler(ctx -> {
+            // 所有的请求都会调用这个处理器处理
+            HttpServerResponse response = ctx.response();
+            response.putHeader("content-type", "text/plain");
+            String execute = redisTemplate.execute(script, Collections.singletonList("k"));
+            // 写入响应并结束处理
+            response.end(execute);
+        });
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ResourceScriptSource pathResource = new ResourceScriptSource(new ClassPathResource("redis/lua.lua"));
+
+        script.setResultType(String.class);
+
+        script.setScriptSource(pathResource);
+    }
+}
